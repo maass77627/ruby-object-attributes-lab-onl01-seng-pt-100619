000c4a5ec3f4ded4216de40e9f4b7bb73fda7093
kelly r maass <kmaass77627@gmail.com>
2019-10-28 08:41:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  
+  fido = Dog.new 
+  
+  def name=(fido)
+    @name = fido 
+    end 
+    
+    def name
+      @name 
+    end 
+    
+    def breed=(beagle)
+      @breed = beagle
+    end
+    
+    def breed
+      @breed
+    end 
+    
+  end 
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person 
+
+new_name = Person.new 
+
+def name=(new_name)
+  @name = new_name 
+end 
+
+def name 
+  @name
+end 
+
+def job=(singer)
+  @job = singer
+end 
+
+def job 
+  @job
+end 
+
+end
